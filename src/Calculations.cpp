16840cc709d4795d600b0aeac7bb544810ebe4a8
Kamaropoulos <kamaropoulos@outlook.com>
2019-01-23 08:43:50 +0200

    feat: Added empty Calculations.cpp file

---
+++ src/Calculations.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Calculations.cpp
+ * @author Konstantinos Kamaropoulos (kamaropoulos@outlook.com)
+ * @brief Methods for data calculations
+ * @version 0.1
+ * @date 2019-01-23
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
